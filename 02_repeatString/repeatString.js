518ba56a1d8ac990f699ab3492f3e077389b7a2a
Hamza Eshoul <hamza.eshoul.pro@gmail.com>
2022-06-02 13:27:53 +0100

    helloWorld.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
 
-};
+const repeatString = function(string, num) {
+
+    let storedString = "";
+
+    for ( i = 1; i <= num ; i++) {
+
+        storedString = string + storedString;
+    };
+
+
+    return storedString;
+
+    };
+    
+  repeatString('',10);
+
 
 // Do not edit below this line
 module.exports = repeatString;
