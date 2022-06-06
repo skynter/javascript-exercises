1297c46cd205f776caa89ee9707fc9ee180ad0fa
Hamza Eshoul <hamza.eshoul.pro@gmail.com>
2022-06-06 08:56:15 +0100

    XDD

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray,myArray2) {
 
-};
+    let removed = myArray.splice(2,1);
+    let removed2 = myArray.splice(1,2)
+
+    return myArray;
+    
+    };
+    
+    removeFromArray([1,2,3,4],3);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
