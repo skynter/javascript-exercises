f06dd8b89e0492f67b689e9417014b156b4fd00c
Hamza Eshoul <hamza.eshoul.pro@gmail.com>
2022-06-23 14:09:28 +0100

    Fibonnaci XD

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,36 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
-};
-
+    let everyFirstNum = 1;
+    let everySecondNum = 1;
+    let myFibo = [1,1];
+    let result = ""
+    
+    if (index <0) {
+        return "OOPS";
+    }
+    else {
+    // Fibonacci sequence 
+    for (let i = 0; i < 10; i++) {
+        
+        sum = everyFirstNum + everySecondNum;
+    
+        everyFirstNum = sum + everySecondNum;
+    
+        everySecondNum = everyFirstNum + sum;
+    
+        result = [sum, everyFirstNum, everySecondNum];
+    
+        myFibo = myFibo.concat(result);
+    
+    };
+    
+    // FindmyIndex code 
+    let myIndex = myFibo[index-1];
+    
+    return myIndex;
+    }
+    };
+    
+   fibonacci();
 // Do not edit below this line
 module.exports = fibonacci;
