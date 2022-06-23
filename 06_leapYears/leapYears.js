5b8a1c16e2e7d505bebe46813cbd8de68ea99bff
Hamza Eshoul <hamza.eshoul.pro@gmail.com>
2022-06-23 10:58:22 +0100

    great

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,32 @@
-const leapYears = function() {
+     const leapYears = function(myYear, isLeapYear) {
 
-};
-
+        if (myYear % 100 !== 0) {
+        
+            if (myYear % 4 === 0) {
+                console.log ("is a leap year");
+                isLeapYear = true;
+            }
+            else {
+                console.log("is not a leap year");
+                isLeapYear = false;
+            }
+            }
+        
+        else if (myYear % 100 === 0) {
+        
+            if (myYear % 100 === 0 && myYear % 400 === 0) {
+                console.log ("is a leap year");
+                isLeapYear = true;
+                }
+            else {
+                console.log ("is not a leap year");
+                isLeapYear = false;
+            }
+            };
+        
+            return isLeapYear;
+        };
+        
+        leapYears(700);
 // Do not edit below this line
 module.exports = leapYears;
